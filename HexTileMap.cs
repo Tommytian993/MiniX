a7798ae17e76f48bbef55f71f53b78149afe7e81
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-06-15 15:12:07 -0400

    feat: add terrain generation to HexTileMap
    - Add GenerateTerrain() method to create base hex grid
    - Implement nested loops for width and height grid generation
    - Call terrain generation in _Ready()

--- HexTileMap.cs
+++ HexTileMap.cs
@@ -3,20 +3,27 @@ using System;
 
 public partial class HexTileMap : Node2D
 {
-     [Export]
-     public int width = 100;
-     [Export]
-     public int height = 60;
+	 [Export]
+	 public int width = 100;
+	 [Export]
+	 public int height = 60;
 
-     TileMapLayer baseLayer, borderLayer, overlayLayer;
+	 TileMapLayer baseLayer, borderLayer, overlayLayer;
 
-     public override void _Ready()
-     {
-        baseLayer = GetNode<TileMapLayer>("BaseLayer");
-        borderLayer = GetNode<TileMapLayer>("HexBordersLayer");
-        overlayLayer = GetNode<TileMapLayer>("SelectionOverlayLayer");
-     }
-     public override void _Process(double delta){
-
+	 public override void _Ready()
+	 {
+		baseLayer = GetNode<TileMapLayer>("BaseLayer");
+		borderLayer = GetNode<TileMapLayer>("HexBordersLayer");
+		overlayLayer = GetNode<TileMapLayer>("SelectionOverlayLayer");
+		GenerateTerrain();
+	 }
+	 public override void _Process(double delta){
+	 }
+     public void GenerateTerrain(){
+          for(int x = 0; x < width; x++){
+		     for(int y = 0; y < height; y++){
+				baseLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
+			}
+		}
      }
 }
